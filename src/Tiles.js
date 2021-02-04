b32d06f8b6f9fd02047cc28cfd3a2ec56a007b5e
Cindy Lee <cindylee@CL-NEW-MacBook-Pro.local>
2021-02-04 00:11:30 -0800

    Added numbers merging feature

--- src/Tiles.js
+++ src/Tiles.js
@@ -6,9 +6,9 @@ const GRID_SIZE = 4;
 class Tiles extends Component {
     state = {
         tiles: [
-            [0,16,4,2],
-            [16,2,8,32],
-            [0,0,4,4],
+            [2,0,2,2],
+            [2,0,0,2],
+            [0,8,4,4],
             [0,2,0,2]
         ]
     }
@@ -33,90 +33,67 @@ class Tiles extends Component {
     }
 
     shiftRight = () => {
-        let tiles = [...this.state.tiles];
+        let updatedTiles = [...this.state.tiles];
+        //let emptyTiles = [];
 
-        for (let i=GRID_SIZE-1; i>=0; i--) {
-            let zeros = 0;
-            /// SHIFT ALL TO THE RIGHT 
-            for (let j=GRID_SIZE-1; j>=0; j--) {
-                if (tiles[i][j] === tiles[i][j-1]) {
-                    tiles[i][j] *= 2;
-                    tiles[i][j-1] = 0;
+        for (let i=0; i<GRID_SIZE; i++) {
+            let zeros = updatedTiles[i][GRID_SIZE-1] === 0? 1:0;
+            let index = GRID_SIZE-1;
+            let prevIndex = GRID_SIZE-1;
+            let prevVal = updatedTiles[i][GRID_SIZE-1];
+            
+            for (let j=GRID_SIZE-2; j>=0; j--) {
+                // When the element is 0
+                if (updatedTiles[i][j] === 0) zeros++;
+
+                // When the element matches with previous value
+                else if (updatedTiles[i][j] === prevVal) {
+                    updatedTiles[i][j] = 0;
+                    updatedTiles[i][prevIndex] = 0;
+                    updatedTiles[i][index] = prevVal * 2;
+                    zeros++;
+                    prevVal = 0;
+                    index = j+zeros;
                 }
-
-                if (tiles[i][j] === 0) zeros++;
-                else {
+                // When the element has different value from previous value
+                else if (updatedTiles[i][j] !== prevVal) {
+                    prevVal = updatedTiles[i][j];
+                    prevIndex = j;
+                    index = j+zeros;
                     if (zeros !== 0) {
-                        tiles[i][j+zeros] = tiles[i][j];
-                        tiles[i][j] = 0;
-                    } 
+                        updatedTiles[i][index] = prevVal;
+                        updatedTiles[i][j] = 0;
+                    }
                 }
             }
         }
-        this.setState({tiles: tiles});
+        this.setState({tiles: updatedTiles});
     }
 
     shiftLeft = () => {
         let updatedTiles = [...this.state.tiles];
-        let emptyTiles = [];
+        //let emptyTiles = [];
 
         for (let i=0; i<GRID_SIZE; i++) {
-            /*let zeros = 0;
-            /// SHIFT ALL TO THE LEFT 
-            for (let j=0; j<GRID_SIZE; j++) {
-                if (tiles[i][j] === 0) zeros++;
-                else if (zeros !== 0) {
-                    tiles[i][j-zeros] = tiles[i][j];
-                    tiles[i][j] = 0; 
-                }
-
-            }
-            zeros = 0;
-            for (let j=0; j<GRID_SIZE-1; j++) {
-                if (tiles[i][j] === tiles[i][j+1]) {
-                    tiles[i][j] *= 2;
-                    tiles[i][j+1] = 0;
-                    
-                    for (let k=j+1; k<GRID_SIZE-1; k++) {
-                        let temp = tiles[i][k];
-                        tiles[i][k] = tiles[i][k+1];
-                        tiles[i][k+1] = temp;
-                    }
-                }
-
-            }*/
-            console.log(updatedTiles[i]);
-            // 0의 갯수
             let zeros = updatedTiles[i][0] === 0? 1:0;
-            
-            // 숫자 있는 곳
+            let index = 0;
             let prevIndex = 0;
-            
-            // 이전 value
             let prevVal = updatedTiles[i][0];
             
-            let index = 0;
-
             for (let j=1; j<GRID_SIZE; j++) {
-                // 0일때
-                if (updatedTiles[i][j] === 0) {
-                    
-                    zeros++;
-                    console.log('zeros = '+zeros);
-                }
-                // 이전 밸류와 같을 때
+                // When the element is 0
+                if (updatedTiles[i][j] === 0) zeros++;
+
+                // When the element matches with previous value
                 else if (updatedTiles[i][j] === prevVal) {
-                    
-                    //if (prevIndex !== j-1) updatedTiles[i][j-1] = 0;
                     updatedTiles[i][j] = 0;
                     updatedTiles[i][prevIndex] = 0;
                     updatedTiles[i][index] = prevVal * 2;
                     zeros++;
                     prevVal = 0;
                     index = j-zeros;
-                    console.log('이전 밸류와 같을때 인덱스 = '+index);
                 }
-                // 이전 밸류와 다를 때
+                // When the element has different value from previous value
                 else if (updatedTiles[i][j] !== prevVal) {
                     prevVal = updatedTiles[i][j];
                     prevIndex = j;
@@ -125,51 +102,86 @@ class Tiles extends Component {
                         updatedTiles[i][index] = prevVal;
                         updatedTiles[i][j] = 0;
                     }
-                    
-                    console.log('이전 밸류와 다를 때 인덱스 = '+index);
                 }
-                
             }
         }
         this.setState({tiles: updatedTiles});
     }
 
     shiftUp = () => {
-        let tiles = [...this.state.tiles];
+        let updatedTiles = [...this.state.tiles];
+        //let emptyTiles = [];
 
         for (let j=0; j<GRID_SIZE; j++) {
-            let zeros = 0;
-            /// SHIFT ALL TO THE LEFT 
-            for (let i=0; i<GRID_SIZE; i++) {
-                if (tiles[i][j] === 0) zeros++;
-                else {
+            let zeros = updatedTiles[0][j] === 0? 1:0;
+            let index = 0;
+            let prevIndex = 0;
+            let prevVal = updatedTiles[0][j];
+            
+            for (let i=1; i<GRID_SIZE; i++) {
+                // When the element is 0
+                if (updatedTiles[i][j] === 0) zeros++;
+
+                // When the element matches with previous value
+                else if (updatedTiles[i][j] === prevVal) {
+                    updatedTiles[i][j] = 0;
+                    updatedTiles[prevIndex][j] = 0;
+                    updatedTiles[index][j] = prevVal * 2;
+                    zeros++;
+                    prevVal = 0;
+                    index = i-zeros;
+                }
+                // When the element has different value from previous value
+                else if (updatedTiles[i][j] !== prevVal) {
+                    prevVal = updatedTiles[i][j];
+                    prevIndex = i;
+                    index = i-zeros;
                     if (zeros !== 0) {
-                        tiles[i-zeros][j] = tiles[i][j];
-                        tiles[i][j] = 0;
-                    } 
+                        updatedTiles[index][j] = prevVal;
+                        updatedTiles[i][j] = 0;
+                    }
                 }
             }
         }
-        this.setState({tiles: tiles});
+        this.setState({tiles: updatedTiles});
     }
 
     shiftDown = () => {
-        let tiles = [...this.state.tiles];
+        let updatedTiles = [...this.state.tiles];
+        //let emptyTiles = [];
 
-        for (let j=GRID_SIZE-1; j>=0; j--) {
-            let zeros = 0;
-            /// SHIFT ALL TO THE RIGHT 
-            for (let i=GRID_SIZE-1; i>=0; i--) {
-                if (tiles[i][j] === 0) zeros++;
-                else {
+        for (let j=0; j<GRID_SIZE; j++) {
+            let zeros = updatedTiles[GRID_SIZE-1][j] === 0? 1:0;
+            let index = GRID_SIZE-1;
+            let prevIndex = GRID_SIZE-1;
+            let prevVal = updatedTiles[GRID_SIZE-1][j];
+            
+            for (let i=GRID_SIZE-2; i>=0; i--) {
+                // When the element is 0
+                if (updatedTiles[i][j] === 0) zeros++;
+
+                // When the element matches with previous value
+                else if (updatedTiles[i][j] === prevVal) {
+                    updatedTiles[i][j] = 0;
+                    updatedTiles[prevIndex][j] = 0;
+                    updatedTiles[index][j] = prevVal * 2;
+                    zeros++;
+                    prevVal = 0;
+                    index = i+zeros;
+                }
+                // When the element has different value from previous value
+                else if (updatedTiles[i][j] !== prevVal) {
+                    prevVal = updatedTiles[i][j];
+                    prevIndex = i;
+                    index = i+zeros;
                     if (zeros !== 0) {
-                        tiles[i+zeros][j] = tiles[i][j];
-                        tiles[i][j] = 0;
-                    } 
+                        updatedTiles[index][j] = prevVal;
+                        updatedTiles[i][j] = 0;
+                    }
                 }
             }
         }
-        this.setState({tiles: tiles});
+        this.setState({tiles: updatedTiles});
     }
     
 
@@ -191,7 +203,7 @@ class Tiles extends Component {
                             return (
                             <tr>
                                 {items.map((subItems, sIndex) => {
-                                    return <th key={index+sIndex}> {subItems} </th>;
+                                    return <th key={index+sIndex}> {subItems===0? null:subItems} </th>;
                                 })}
                             </tr>
                             );
